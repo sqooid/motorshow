c65d833c3daba17e2ea549c1271979319f942a17
Lucas Liu <sqooiddev@gmail.com>
2023-01-12 14:39:12 +1100

    added more bikes

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -43,6 +43,72 @@ export const bikeList: Make[] = [
         cc: 399,
         topSpeed: 160,
         msrp: 8300
+      },
+      {
+        id: 'ninja-650',
+        name: 'Ninja 650',
+        smallImage:
+          'https://content2.kawasaki.com/ContentStorage/KMA/Products/12035/98efeea6-ae2b-4aa6-8c70-d2f2ea2b4122.jpg',
+        largeImage:
+          'https://content2.kawasaki.com/ContentStorage/KMA/Products/12035/98efeea6-ae2b-4aa6-8c70-d2f2ea2b4122.jpg',
+        horsepower: 49,
+        cc: 399,
+        topSpeed: 160,
+        msrp: 8300
+      },
+      {
+        id: 'z400',
+        name: 'Z400',
+        smallImage:
+          'https://content2.kawasaki.com/ContentStorage/KMA/Products/11990/53e562f9-ac91-4026-845a-05e96b73048c.png',
+        largeImage:
+          'https://content2.kawasaki.com/ContentStorage/KMA/Products/11990/53e562f9-ac91-4026-845a-05e96b73048c.png',
+        horsepower: 49,
+        cc: 399,
+        topSpeed: 160,
+        msrp: 8300
+      },
+      {
+        id: 'z650',
+        name: 'Z650',
+        smallImage:
+          'https://content2.kawasaki.com/contentstorage/KMA/Enhanced360/45/GY1/l_size/0_15.jpg',
+        largeImage:
+          'https://content2.kawasaki.com/contentstorage/KMA/Enhanced360/45/GY1/l_size/0_15.jpg',
+        horsepower: 49,
+        cc: 399,
+        topSpeed: 160,
+        msrp: 8300
+      }
+    ]
+  },
+  {
+    id: 'suzuki',
+    name: 'Suzuki',
+    models: [
+      {
+        id: 'gsx-s125',
+        name: 'GSX-S125',
+        smallImage:
+          'https://www.suzukimotorcycles.com.au/wp-content/uploads/2021/06/GSX-S125XAM1_YVU_Right-915x612.jpg',
+        largeImage:
+          'https://www.suzukimotorcycles.com.au/wp-content/uploads/2022/07/GSX-S125XAM1_RunOut_Offer.jpg',
+        horsepower: 0,
+        cc: 125,
+        topSpeed: 0,
+        msrp: 5440
+      },
+      {
+        id: 'sv650',
+        name: 'SV650',
+        smallImage:
+          'https://www.suzukimotorcycles.com.au/wp-content/uploads/2022/12/SV650AM3_BTH_Right.jpg',
+        largeImage:
+          'https://www.suzukimotorcycles.com.au/wp-content/uploads/2022/12/SV650AM3_BTH_Right.jpg',
+        horsepower: 0,
+        cc: 125,
+        topSpeed: 0,
+        msrp: 5440
       }
     ]
   }
--- src/routes/gallery/grid.svelte
+++ src/routes/gallery/grid.svelte
@@ -1,3 +1,3 @@
-<div class="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 ease-out">
+<div class="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5">
   <slot />
 </div>
